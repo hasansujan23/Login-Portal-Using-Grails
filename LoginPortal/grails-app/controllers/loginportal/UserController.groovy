3d14342d494dc29ba0ff2a96228dcfa9659e6137
hasansujan23 <hasansujan23@gmail.com>
2019-11-07 13:34:31 +0600

    create view and modified user controller

--- LoginPortal/grails-app/controllers/loginportal/UserController.groovy
+++ LoginPortal/grails-app/controllers/loginportal/UserController.groovy
@@ -4,10 +4,22 @@ class UserController {
 
     def index() {
     	if(session.user==null){
-    		
+    		redirect(action:"login")
     	}
     	else{
-    		
+    		redirect(action:"profile")
     	}
+    }
+
+    def login(){
+
+    }
+
+    def registration(){
+
+    }
+
+    def profile(){
+
     }
 }
